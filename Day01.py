ecc250432fb90856f590da7969fe509c7e228586
NightShadowSnow <120093561+NightShadowSnow@users.noreply.github.com>
2022-12-08 10:48:48 +0100

    Add files via upload

---
+++ Day01.py
@@ -0,0 +1,26 @@
+with open('01.in') as file:
+    data = [i for i in file.read().strip().split("\n")]
+
+max = 0
+max2 = 0
+max3 = 0
+count = 0
+for item in data:
+    if item == '':
+        count = 0  
+    else:
+        num = int(item)
+        count += num
+
+    if count > max:
+        max3 = max2
+        max2 = max
+        max = count
+    elif count > max2:
+        max3 = max2
+        max2 = count
+    elif count > max3:
+        max3 = count
+
+print("Answer to part 1: ", max)
+print("Answer to part 2: ", max+max2+max3)
